454b0d9f761ec263d6831f832e2398fd34af880c
Randy Sum <randy26smc@gmail.com>
2023-09-26 23:12:23 -0600

    agregada la vista principal

--- client/src/components/TaskForm.js
+++ client/src/components/TaskForm.js
@@ -1,6 +1,100 @@
- 
+/*Se utiliza para ordenar elementos dentro de la pantalla */
+import {
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
+
 export default function TaskForm() {
+  const navigate = useNavigate();
+
   return (
-    <div>TaskForm</div>
-  )
+    <Grid
+      container
+      direction="column"
+      alignItems="center"
+      justifyContent="center"
+    >
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }}
+          style={{ backgroundColor: "white", padding: "1rem" }}
+        >
+          <Typography variant="5" textAlign="center" color="green">
+            Create Task
+          </Typography>
+          <CardContent>
+            <form>
+              <TextField
+                variant="filled"
+                label="Escribe tu nombre"
+                placeholder="testing"
+                sx={{ display: "block", margin: ".5rem 0" 
+              }}
+                inputProps={{style:{color:"white"}}}
+                InputLabelProps={{style:{color:"white"}}}
+              />
+
+
+              <TextField
+                variant="filled"
+                label="Contraseña"
+                /**
+                 * multiline
+                 * rows={4} para que sea multilinea
+                 */
+                sx={{ display: "block", margin: ".5rem 0" }}
+              />
+            </form>
+            <Button variant="contained" color="secondary" type="submit">
+              Save
+            </Button>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      <Grid item xs={3}>
+        <Card
+          sx={{ mt: 5 }}
+          style={{ backgroundColor: "white", padding: "1rem" }}
+        >
+          <Typography variant="5" textAlign="center" color="green">
+            Create Task
+          </Typography>
+          <CardContent>
+            <form>
+              <TextField
+                variant="filled"
+                label="Escribe tu nombre"
+                placeholder="testing"
+                sx={{ display: "block", margin: ".5rem 0" 
+              }}
+                inputProps={{style:{color:"white"}}}
+                InputLabelProps={{style:{color:"white"}}}
+              />
+
+
+              <TextField
+                variant="filled"
+                label="Contraseña"
+                /**
+                 * multiline
+                 * rows={4} para que sea multilinea
+                 */
+                sx={{ display: "block", margin: ".5rem 0" }}
+              />
+            </form>
+            <Button variant="contained" color="secondary" type="submit">
+              Save
+            </Button>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+    
+  );
 }
